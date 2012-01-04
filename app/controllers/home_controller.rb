64462c14716214ae34f13de25b8e113eee6be953
Chris-Sturgess <christopher.sturgess@gmail.com>
2012-01-04 14:38:47 -0500

    Updated to render to jsonp

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
+    #placeholder, for now
+  	@items = { @yes => true, @test => "something" }
+		
+		respond_to do |format|
+			format.html # index.html.erb
+			format.json { render :json => @items, :callback => params[:callback] }
+		end
   end
 
 end

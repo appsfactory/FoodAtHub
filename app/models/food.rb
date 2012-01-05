9a5542f2503ee87dd59b17eb3d56625085f55340
Rob Drimmie <rob@drimmie.net>
2012-01-05 13:48:15 -0500

    Minor modification of the model, methods need to be defined against self.

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
 class Food < ActiveRecord::Base
 
-	def checkTimeExpiry?
+	def self.checkTimeExpiry?
 	@food = Food.last
 		c = self.where('updated_at >= ?', 5.seconds.ago).count
 		c > 0 ? true : false

c4cc6c125947874ec1d176e494ff3781b60d1e4a
Christopher Sturgess <christopher.sturgess@gmail.com>
2012-01-05 15:46:02 -0500

    Allowed all names in the layout to be changed when the page is loaded, so that all wording on the page can be changed whenever, and updated the file with full comments

--- Other/www/Sencha/app.js
+++ Other/www/Sencha/app.js
@@ -1,40 +1,55 @@
+/*The app as of Jan. 5.
+TODO:
+Fix the layout (Mostly bugs with the width
+Have all the initial variables fetch their data from the server
+Give buttons the proper functionality
+Decide whether to remove most containers, or keep them as is.
+*/
 var App = new Ext.Application({
     name : 'FoodAtTheHub',
     useLoadMask : true,
     launch : function () {		
-
+				//These are the initial variables that will hold the data to display on the webpage. Currently static, they should soon be updated to change to the appropriate setting when the page loads
 				var	foodString = 'NO';
 				var timeString = 'There has been no food for over 9000 seconds';
-
+				var firstButtonString = "I'm a button!";
+				var secondButtonString = "I'm a button, too!";
+				var foodToolBarString = 'FoodAtTheHub.com';
+				
+				//The toolbar at the top of the screen, mostly decorative.
 				FoodAtTheHub.views.foodToolbar = new Ext.Toolbar({
 						id: 'foodToolbar',
-						title: 'FoodAtTheHub.com',
+						title: foodToolBarString,
 						height : 46,
 						width : 320,
 						maxWidth : 320
 				});
 
+				//The first button to display
 				FoodAtTheHub.views.firstButton = new Ext.Button({
 						id : 'firstButton',
-						text : "I'm a button!",
+						text : firstButtonString,
 						scale : 'small',
 						width : 80,
 						height : 70
 				});
-
+				
+				//The second button to display
 				FoodAtTheHub.views.secondButton = new Ext.Button({
 						id : 'secondButton',
-						text : "Me too!",
+						text : secondButtonString,
 						scale : 'small',
 						width : 80,
 						height : 70
 				});
 		
+				//A buffer to keep things in line. I'm pretty sure there's an easier way, so this is probably just temporary
 				FoodAtTheHub.views.buffer = new Ext.Panel({
 						id : 'buffer',
 						height : 30
 				});
 
+				//This is a container to keep the buttons in check.
 				FoodAtTheHub.views.buttonContainer = new Ext.Panel({
 						id : 'buttonContainer',						
 						width : 200,
@@ -42,6 +57,7 @@ var App = new Ext.Application({
 						dockedItems : [FoodAtTheHub.views.firstButton, FoodAtTheHub.views.buffer, FoodAtTheHub.views.secondButton]
 				});
 
+				//The first text container, contains only the yes/no answer
 				FoodAtTheHub.views.textContainer = new Ext.Panel({
 						id : 'textContainer',						
 						html : '<DIV align="center"><p style="font-size:500%">'.concat(foodString, '</p></DIV>'),
@@ -49,6 +65,7 @@ var App = new Ext.Application({
 						height: 84
 				});
 
+				//The second text container, contains the time string
 				FoodAtTheHub.views.textContainer2 = new Ext.Panel({
 						id : 'textContainer2',						
 						html : '<DIV align="center"><p>'.concat(timeString, '</p></DIV>'),
@@ -56,6 +73,7 @@ var App = new Ext.Application({
 						height : 60
 				});
 
+				//The food container, contains everything with proper buffers to seperate
 				FoodAtTheHub.views.foodContainer = new Ext.Panel({
 						id : 'foodContainer',	
 						height: 480,
@@ -63,6 +81,7 @@ var App = new Ext.Application({
 						dockedItems : [FoodAtTheHub.views.foodToolbar, FoodAtTheHub.views.textContainer, FoodAtTheHub.views.buffer, FoodAtTheHub.views.textContainer2,FoodAtTheHub.views.buffer,  FoodAtTheHub.views.buttonContainer]
 				});
 				
+				//The main view, displays the food containers
 				FoodAtTheHub.views.viewport = new Ext.Panel({
 						id : 'viewPort',						
 						maxWidth : 320,

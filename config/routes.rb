da0da36e91b37a2849b11d3c078fe2e4b70f62ca
Debbie Chen <debbie.chen@communitech.ca>
2012-02-09 15:31:25 -0500

    Changed root route to home#client for demo purposes

--- config/routes.rb
+++ config/routes.rb
@@ -59,7 +59,10 @@ FoodAtHub::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  
+  #original
+  #root :to => 'home#index'
+  root :to => 'home#client'
 
   # See how all your routes lay out with "rake routes"
 
